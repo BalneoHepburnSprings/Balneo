6ff407a38f1e340aacd4967281b58350bb7a5ca3
BalneoHepburnSprings <balneohepburnsprings@gmail.com>
2025-06-21 18:31:08 +1000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle("open");
     });
 });
+
+
+// Prevent hamburger menu from triggering when clicking social icons
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".top-bar .social a").forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.stopPropagation();
+        });
+    });
+});
